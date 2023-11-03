a1dfdc413d064337af66a5f9bbcd569e5a3a3f7c
keep-you-busy <twiddle_thumbs@aol.com>
2023-11-03 20:57:21 +0300

    API viewsets

--- notification_backend/api/views.py
+++ notification_backend/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from api.serializer import ClientSerializer, NewsLetterSerializer, MessageSerializer
+from users.models import Client, NewsLetter, Message
+
+
+class ClientViewSet(ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+
+class NewsLetterViewSet(ModelViewSet):
+    queryset = NewsLetter.objects.all()
+    serializer_class = NewsLetterSerializer
+
+
+class MessageViewSet(ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer

0a28200f6225fbd3869d12270dd2d69a2dca5819
keep-you-busy <twiddle_thumbs@aol.com>
2023-11-08 14:31:57 +0300

    Client serializer: time zone, read only network code

--- notification_backend/api/serializer.py
+++ notification_backend/api/serializer.py
@@ -1,13 +1,22 @@
+import pytz
 from rest_framework import serializers
 
 from users.models import Client, NewsLetter, Message
 
 
 class ClientSerializer(serializers.ModelSerializer):
+    time_zone = serializers.ChoiceField(choices=pytz.all_timezones)
 
     class Meta:
         model = Client
-        fields = '__all__'
+        fields = (
+            'id',
+            'phone_number',
+            'network_code',
+            'tag',
+            'time_zone'
+        )
+        read_only_fields = ('network_code',)
 
 
 class NewsLetterSerializer(serializers.ModelSerializer):

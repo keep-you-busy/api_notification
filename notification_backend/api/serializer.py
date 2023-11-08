213f622f69886e09632a0356302213a42f91d305
keep-you-busy <twiddle_thumbs@aol.com>
2023-11-08 18:02:55 +0300

    Serializer for list of newsletters

--- notification_backend/api/serializer.py
+++ notification_backend/api/serializer.py
@@ -2,7 +2,7 @@ import pytz
 from rest_framework import serializers
 
 from users.models import Client, NewsLetter, Message
-
+from api.utils import set_clients_values
 
 class ClientSerializer(serializers.ModelSerializer):
     time_zone = serializers.ChoiceField(choices=pytz.all_timezones)
@@ -20,10 +20,24 @@ class ClientSerializer(serializers.ModelSerializer):
 
 
 class NewsLetterSerializer(serializers.ModelSerializer):
+    tags = serializers.SerializerMethodField()
+    network_codes = serializers.SerializerMethodField()
 
     class Meta:
         model = NewsLetter
-        fields = '__all__'
+        fields = (
+            'id',
+            'start_datetime',
+            'end_datetime',
+            'tags',
+            'network_codes'
+        )
+
+    def get_tags(self, newsletter):
+        return set_clients_values(newsletter, 'tag')
+
+    def get_network_codes(self, newsletter):
+        return set_clients_values(newsletter, 'network_code')
 
 
 class MessageSerializer(serializers.ModelSerializer):

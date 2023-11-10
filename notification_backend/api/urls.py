07a427dbea29ba3dad3d8327f766b34d0c287ead
keep-you-busy <twiddle_thumbs@aol.com>
2023-11-10 16:08:06 +0300

    Excess messages url

--- notification_backend/api/urls.py
+++ notification_backend/api/urls.py
@@ -1,13 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api.views import ClientViewSet, MessageViewSet, NewsLetterViewSet
+from api.views import ClientViewSet, NewsLetterViewSet
 
 
 router_v1 = routers.DefaultRouter()
 
 router_v1.register(r'clients', ClientViewSet, basename='clients')
-router_v1.register(r'messages', MessageViewSet, basename='messages')
 router_v1.register(r'newsletters', NewsLetterViewSet, basename='newsletters')
 
 urlpatterns = [

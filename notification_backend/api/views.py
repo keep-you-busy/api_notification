7ddfb9f83161d4065830ed578434b0778155911e
keep-you-busy <twiddle_thumbs@aol.com>
2023-11-10 16:11:07 +0300

    Actions more DRY now

--- notification_backend/api/views.py
+++ notification_backend/api/views.py
@@ -1,13 +1,18 @@
 from rest_framework.viewsets import ModelViewSet
-from api.serializer import ClientSerializer, NewsLetterSerializer, NewsLetterCreateSerializer, MessageSerializer
+from api.serializer import ClientSerializer, NewsLetterSerializer, NewsLetterCreateSerializer
 from users.models import Client, NewsLetter, Message
 from rest_framework.permissions import SAFE_METHODS
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+
+from api.utils import retrieve_messages_statistic
 
 
 class ClientViewSet(ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
-    http_method_names = ['get', 'post', 'patch', 'delete']
+    http_method_names = ('get', 'post', 'patch', 'delete')
 
 
 class NewsLetterViewSet(ModelViewSet):
@@ -22,9 +27,26 @@ class NewsLetterViewSet(ModelViewSet):
             self.serializer_class = NewsLetterCreateSerializer
         elif method == 'PATCH':
             self.serializer_class = NewsLetterCreateSerializer
+
         return self.serializer_class
 
+    @action(methods=('GET',),
+            detail=False,
+            url_path='statistic',
+            url_name='statistic')
+    def get_statistic(self, request):
+        response = retrieve_messages_statistic(
+            Message.objects)
+        response['total_newsletters'] = NewsLetter.objects.count()
 
-class MessageViewSet(ModelViewSet):
-    queryset = Message.objects.all()
-    serializer_class = MessageSerializer
+        return Response(response, status=status.HTTP_200_OK)
+
+    @action(methods=('GET',),
+            detail=True,
+            url_path='statistic',
+            url_name='statistic')
+    def get_detailed_statistic(self, request, pk):
+        response = retrieve_messages_statistic(
+            Message.objects.filter(newsletter=pk))
+
+        return Response(response, status=status.HTTP_200_OK)
